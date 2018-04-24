04c97a23d9a284bcaf998cd344d0162ec32bdd7a
Sebastian Bastidas <sbastidasr@gmail.com>
2018-04-24 11:04:46 -0400

    Refactor

--- public/index.js
+++ public/index.js
@@ -1,34 +1,68 @@
-const YELP_SEARCH_URL = 'http://localhost:3000/asd';
+const YELP_SEARCH_URL = "http://localhost:3000/asd";
 
 var map, infoWindow;
+
+////////////////////// SETUP //////////////////////
 
 function init() {
   autoComplete();
   coffeeSearch();
   renderSearchAgain();
+  infowindow = new google.maps.InfoWindow({
+    content: "__"
+  });
 }
+
 // Autocomplete search location in form
 function autoComplete() {
   let options = {
-    types: ['(regions)']
+    types: ["(regions)"]
   };
 
-  let searchInput = document.getElementById('search-term');
+  let searchInput = document.getElementById("search-term");
   let autocomplete = new google.maps.places.Autocomplete(searchInput, options);
 }
 
+function coffeeSearch() {
+  $(".search-form").on("click", ".submit-button", function(event) {
+    event.preventDefault();
+    const queryTarget = $(".search-query");
+    const locationString = queryTarget.val();
+    getLatLong(locationString);
+    queryTarget.val("");
+    displayResultsScreen();
+  });
+}
+
+function renderSearchAgain() {
+  $(".search-bar-form").on("click", ".submit-button", function(event) {
+    event.preventDefault();
+    const queryTarget = $(".search-bar-query");
+    const locationString = queryTarget.val();
+    getLatLong(locationString);
+    queryTarget.val("");
+  });
+}
+
+function displayResultsScreen() {
+  $(".display-start").remove();
+  $("#map").removeClass("hidden");
+  $("#side-bar").removeClass("hidden");
+}
+
+////////////////////// API //////////////////////
+
 function getDataFromApi(lat, lng, callback) {
-  console.log("hi");
   const settings = {
     url: YELP_SEARCH_URL,
     data: {
       latitude: lat,
       longitude: lng
     },
-    dataType: 'json',
-    type: 'GET',
+    dataType: "json",
+    type: "GET",
     success: callback,
-    error: function (error) {
+    error: function(error) {
       console.log("error", error);
     }
   };
@@ -37,59 +71,25 @@ function getDataFromApi(lat, lng, callback) {
 
 function renderGoogleMaps(lat, lng) {
   let mapOptions = {
-    center: {lat: lat, lng: lng},
+    center: { lat: lat, lng: lng },
     zoom: 12.8,
     zoomControl: true
   };
-  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
 
-//////////////////////////////////////
-
-function renderSearchAgain() {
-  // autoComplete();
-  $('.search-bar-form').on('click', '.submit-button', function (event) {
-    console.log("works");
-    event.preventDefault();
-    const queryTarget = $('.search-bar-query');
-    const locationString = queryTarget.val();
-    getLatLong(locationString);
-    queryTarget.val("");
-
-    infowindow = new google.maps.InfoWindow({
-      content: "__"
-    });
-  });
-}
+////////////////////// Render //////////////////////
 
 function renderResult(business) {
-  let resultShops = `
+  return `
     <div class="side-bar-content">
       <h2>${business.name}</h2>
     </div>
     `;
-
-  return resultShops;
 }
 
-function displayYelpSearchData(data) {
-  let results = "";
-  for (let i = 0; i < data.businesses.length; i++) {
-
-    let business = data.businesses[i];
-    let busLat = data.businesses[i].coordinates.latitude;
-    let busLng = data.businesses[i].coordinates.longitude;
-
-    // console.log(business);
-
-    // ADD MARKER
-    let marker = new google.maps.Marker({
-      position: {lat: busLat, lng: busLng},
-      map: map,
-      title: business.name
-    });
-
-    let contentString = `
+function renderContentString(business) {
+  return `
       <div id="business-info">
         <h1 class=business-name">${business.name}</h1>
         <a class="logo" href="${business.url}" onclick="window.open(this.href);
@@ -103,56 +103,46 @@ function displayYelpSearchData(data) {
         </div>
       </div>
       `;
-
-    marker.addListener('click', function(){
-      infowindow.setContent(contentString);
-      infowindow.open(map, marker);
-    });
-
-    // DISPLAY TO SIDE BAR
-    // results
-    let renderedItem = renderResult(business);
-    results += renderedItem;
-  }
-  // $('.title-content').html(renderTitle());
-  $('.text-content').html(results);
 }
 
+function createMarker(business) {
+  let marker = new google.maps.Marker({
+    position: {
+      lat: business.coordinates.latitude,
+      lng: business.coordinates.longitude
+    },
+    map: map,
+    title: business.name
+  });
+
+  let contentString = renderContentString(business);
+
+  marker.addListener("click", function() {
+    infowindow.setContent(contentString);
+    infowindow.open(map, marker);
+  });
+}
+
+function displayYelpSearchData(data) {
+  var results = data.businesses.map(business => {
+    createMarker(business);
+    return renderResult(business);
+  });
+
+  $(".text-content").html(results);
+}
 
 function getLatLong(locationString) {
-  console.log(locationString);
   let geocoder = new google.maps.Geocoder();
-  geocoder.geocode( { 'address': locationString}, function(results, status) {
-    if (status == 'OK') {
+  geocoder.geocode({ address: locationString }, function(results, status) {
+    if (status == "OK") {
       let lat = results[0].geometry.location.lat();
       let lng = results[0].geometry.location.lng();
-      console.log(lat, lng);
       getDataFromApi(lat, lng, displayYelpSearchData);
       renderGoogleMaps(lat, lng);
     } else {
-      console.log(results);
       console.log(status);
-      alert('Geocode was not successful for the following reason: ' + status);
+      alert("Search failed, search for a different location");
     }
   });
 }
-
-function coffeeSearch() {
-  $('.search-form').on('click', '.submit-button', function (event) {
-    event.preventDefault();
-    const queryTarget = $('.search-query');
-    const locationString = queryTarget.val();
-    getLatLong(locationString);
-    queryTarget.val("");
-    $('.display-start').remove();
-    $('#map').removeClass('hidden');
-    $('#side-bar').removeClass('hidden');
-
-    infowindow = new google.maps.InfoWindow({
-      content: "__"
-    });
-  });
-
-}
-
-// $(coffeeSearch);
