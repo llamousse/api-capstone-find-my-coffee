c15d24cd3748a5b2947f9f95a3c08f52ff467c3c
Vicky <vyue013@gmail.com>
2018-04-29 02:34:39 -0700

    edit server

--- server.js
+++ server.js
@@ -43,6 +43,12 @@ app.get("/asd", function(req, res) {
 var staticPath = path.join(__dirname, "/public");
 app.use(express.static(staticPath));
 
-app.listen(3000, function() {
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
   console.log("listening");
 });
+
+// app.listen(3000, function() {
+//   console.log("listening");
+// });
